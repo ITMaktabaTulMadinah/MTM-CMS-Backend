3c54d1b90cbd375488725b7b58d1a8b6ff0cb7e2
ITMaktabaTulMadinah <asst.it@maktabatulmadinah>
2025-11-03 16:01:00 +0500

    cors setting update

--- index.js
+++ index.js
@@ -33,7 +33,13 @@ export const io = new Server(server, {
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(morgan("dev"));
 io.use(socketAuth);
 
